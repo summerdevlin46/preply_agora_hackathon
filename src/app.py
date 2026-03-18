9810305146a658c1482e8264fdb44413adbaa6c4
Zappandy <nanoandres_24@hotmail.com>
2026-03-18 18:26:29 +0100

    removed old components and moved to ui to orchestrate with app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import gradio as gr
 import typer
+from mirror.ui.components import build_exercise_section, build_worksheet_section
+from mirror.utils.ocr import parse_worksheet
 
 app = typer.Typer()
 
--- src/mirror/og_old_components.py
+++
@@ -1,2 +0,0 @@
-from mirror.ui.components import build_exercise_section, build_worksheet_section
-from mirror.utils.ocr import parse_worksheet
