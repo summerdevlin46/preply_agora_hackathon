50f0e77c28897641a692c3457bf88e0996823981
Zappandy <nanoandres_24@hotmail.com>
2026-03-20 08:35:11 +0100

    new version and fixed test imports

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import typer
 
 from mirror.ui.components import build_exercise_section, build_worksheet_section
-from mirror.utils.ocr import parse_worksheet
+from mirror.ocr import parse_worksheet
 
 app = typer.Typer()
 
